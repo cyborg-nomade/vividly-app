89ea9aa3bab11ba14614ee8811c5c5d0f1bf3c63
Uriel Fiori <uriel.fiori@gmail.com>
2021-08-17 11:42:22 -0300

    attribute routing

--- vividly-app/Controllers/MovieController.cs
+++ vividly-app/Controllers/MovieController.cs
@@ -19,6 +19,7 @@ namespace vividly_app.Controllers
             return View(movie);
         }
 
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
