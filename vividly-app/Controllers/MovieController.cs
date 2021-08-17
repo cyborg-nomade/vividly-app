77b84a30af96f124fe7d994221c5c8151c134574
Uriel Fiori <uriel.fiori@gmail.com>
2021-08-17 11:36:14 -0300

    convention-based routing

--- vividly-app/Controllers/MovieController.cs
+++ vividly-app/Controllers/MovieController.cs
@@ -19,24 +19,9 @@ namespace vividly_app.Controllers
             return View(movie);
         }
 
-        public ActionResult Edit(int id)
+        public ActionResult ByReleaseDate(int year, int month)
         {
-            return Content("id=" + id);
-        }
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
-
-            if (string.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
-
-            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+            return Content(year + "/" + month);
         }
     }
 }
