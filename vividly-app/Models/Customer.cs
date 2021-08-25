3778208e0259fc994780f31dc23c57086460841f
Uriel Fiori <uriel.fiori@gmail.com>
2021-08-24 21:23:23 -0300

    custom validation

--- vividly-app/Models/Customer.cs
+++ vividly-app/Models/Customer.cs
@@ -9,13 +9,14 @@ namespace vividly_app.Models
     public class Customer
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please enter customer's name")]
         [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
+        [Min18YearsIfAMember]
         public DateTime? Birthday { get; set; }
     }
 }
