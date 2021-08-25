54d8a262f049f28f26a875cf74544fa241baa261
Uriel Fiori <uriel.fiori@gmail.com>
2021-08-25 17:09:31 -0300

    initializing api

---
+++ vividly-app/Controllers/API/CustomersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using vividly_app.Models;
+
+namespace vividly_app.Controllers.API
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+
+    }
+}
