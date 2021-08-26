d33e7f2f8d98ccacb1ad5f2c2f77524cd933faba
Uriel Fiori <uriel.fiori@gmail.com>
2021-08-26 19:34:32 -0300

    implemented movies controller

--- vividly-app/App_Start/MappingProfile.cs
+++ vividly-app/App_Start/MappingProfile.cs
@@ -10,6 +10,8 @@ namespace vividly_app
         {
             Mapper.CreateMap<Customer, CustomerDto>();
             Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MovieDto, Movie>();
         }
     }
 }
