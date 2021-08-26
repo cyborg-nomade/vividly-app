6f57dbf0a03217d070e091fb566af1488839dd89
Uriel Fiori <uriel.fiori@gmail.com>
2021-08-26 19:17:22 -0300

    added movie DTO

---
+++ vividly-app/DTOs/MovieDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using vividly_app.Models;
+
+namespace vividly_app.DTOs
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Genre")]
+        public byte GenreId { get; set; }
+        [Required]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+        [Display(Name = "Date Added")]
+        public DateTime DateAdded { get; set; }
+        [Required]
+        [Display(Name = "Number in Stock")]
+        [Range(1, 20)]
+        public short NumberInStock { get; set; }
+    }
+}
