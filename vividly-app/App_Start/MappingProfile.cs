70e09ea6d5d3dd1a290a30d655cae06d6c2d0722
Uriel Fiori <uriel.fiori@gmail.com>
2021-08-27 15:50:44 -0300

    returning hierarchical data
    plus minor corrections in the mapping profile

--- vividly-app/App_Start/MappingProfile.cs
+++ vividly-app/App_Start/MappingProfile.cs
@@ -8,10 +8,14 @@ namespace vividly_app
     {
         public MappingProfile()
         {
-            Mapper.CreateMap<Customer, CustomerDto>().ForMember(c => c.Id, opt => opt.Ignore());
-            Mapper.CreateMap<CustomerDto, Customer>();
-            Mapper.CreateMap<Movie, MovieDto>().ForMember(m => m.Id, opt => opt.Ignore());
-            Mapper.CreateMap<MovieDto, Movie>();
+            // Domain to DTO
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MembershipType, MembershipTypeDto>();
+
+            // DTO to Domain
+            Mapper.CreateMap<CustomerDto, Customer>().ForMember(c => c.Id, opt => opt.Ignore());
+            Mapper.CreateMap<MovieDto, Movie>().ForMember(m => m.Id, opt => opt.Ignore());
         }
     }
 }
---
+++ vividly-app/DTOs/MembershipTypeDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace vividly_app.DTOs
+{
+    public class MembershipTypeDto
+    {
+        public byte Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
