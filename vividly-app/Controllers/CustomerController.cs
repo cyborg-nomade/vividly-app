947c2f615aeb3858653f006b7282242c707dd1f0
Uriel Fiori <uriel.fiori@gmail.com>
2021-08-27 12:51:04 -0300

    datatables with ajax source

--- vividly-app/Controllers/CustomerController.cs
+++ vividly-app/Controllers/CustomerController.cs
@@ -25,10 +25,7 @@ namespace vividly_app.Controllers
         // GET: Customer
         public ActionResult Index()
         {
-
-            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
-
-            return View(customers);
+            return View();
         }
 
         public ActionResult Details(int id)
