4b214e6ef90cae27da062d5a5295a5db48627bad
Uriel Fiori <uriel.fiori@gmail.com>
2021-09-01 19:45:23 -0300

    domain modelling

--- vividly-app/DTOs/RentalDto.cs
+++ vividly-app/DTOs/RentalDto.cs
@@ -1,6 +1,10 @@
-﻿namespace vividly_app.Controllers.API
+﻿using System.Collections.Generic;
+
+namespace vividly_app.DTOs
 {
     public class RentalDto
     {
+        public int CustomerId { get; set; }
+        public List<int> MovieIds { get; set; }
     }
 }
