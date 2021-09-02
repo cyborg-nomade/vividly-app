19b28ce81ce5d7a622dde8134e043b656b3094e3
Uriel Fiori <uriel.fiori@gmail.com>
2021-09-01 21:07:35 -0300

    edge cases

--- vividly-app/Controllers/API/RentalsController.cs
+++ vividly-app/Controllers/API/RentalsController.cs
@@ -23,17 +23,16 @@ namespace vividly_app.Controllers.API
         //[Authorize(Roles = RoleName.CanManageMovies)]
         public IHttpActionResult CreateRental(RentalDto rentalDto)
         {
-            //if (!ModelState.IsValid)
-            //{
-            //    return BadRequest();
-            //}
-
             var customer = _context.Customers.Single(c => c.Id == rentalDto.CustomerId);
 
-            var movies = _context.Movies.Where(m => rentalDto.MovieIds.Contains(m.Id));
+            var movies = _context.Movies.Where(m => rentalDto.MovieIds.Contains(m.Id)).ToList();
 
             foreach (var movie in movies)
             {
+                if (movie.NumberAvailable == 0)
+                {
+                    return BadRequest("Movie is not available");
+                }
                 movie.NumberAvailable--;
 
                 var rental = new Rental
